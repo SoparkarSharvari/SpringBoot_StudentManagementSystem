c3959a3b9ddffff3e9b8626d1c3cf082ef1f5dc2
SoparkarSharvari <sharvarisoparkar@gmail.com>
2024-11-28 12:58:44 -0800

    first commit

--- src/main/java/com/example/Management/system/model/User.java
+++ src/main/java/com/example/Management/system/model/User.java
@@ -28,20 +28,8 @@ public class User {
         this.name = name;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
